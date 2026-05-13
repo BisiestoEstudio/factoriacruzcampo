2c3ffd4fa17fba4642a4a4a65c92b70079557b52
eletitos <36730057+eletitos@users.noreply.github.com>
2026-05-13 17:38:54 +0200

    Coorporativo template 2

--- wp-content/plugins/factoria-cruzcampo-blocks/src/blocks/corporativo/templates/template_2.php
+++ wp-content/plugins/factoria-cruzcampo-blocks/src/blocks/corporativo/templates/template_2.php
@@ -3,11 +3,9 @@ $gif_url   = FCB_PLUGIN_URL . 'assets/images/gif_cerveza.gif';
 
 ?>
 
-<div class="b-corporativo-1 has-red-background-color alignfull has-global-padding has-white-color">
-	<div class="b-corporativo-1__inner alignexpand">
-        <span>FACTORÍA</span>
-        <img class="b-custom-title__gif" src="<?php echo esc_url($gif_url); ?>"
-            alt="gif decorativo de brindis con cerveza" aria-hidden="true" width="16" height="16" />
+<div class="b-corporativo-2 alignwide has-global-padding has-red-color has-display-s-font-size">
+        <span >FACTORÍA</span>
+        <img class="b-corporativo-2__image" src="<?php echo esc_url($gif_url); ?>"
+            alt="gif decorativo de brindis con cerveza" aria-hidden="true" width="347" height="286" />
         <span>CRUZCAMPO</span>
-    </div>
 </div>
