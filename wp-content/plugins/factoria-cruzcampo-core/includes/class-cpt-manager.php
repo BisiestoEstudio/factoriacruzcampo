9434f927b85d8f4f31b5334f5fc77dc095f6d8dc
eletitos <36730057+eletitos@users.noreply.github.com>
2026-06-18 16:44:05 +0200

    Ajustes varios. Registrar experiencias en menús

--- wp-content/plugins/factoria-cruzcampo-core/includes/class-cpt-manager.php
+++ wp-content/plugins/factoria-cruzcampo-core/includes/class-cpt-manager.php
@@ -9,22 +9,25 @@ class FCC_CPT_Manager {
 
 	private static function register_experience() {
 		$labels = array(
-			'name'               => _x( 'Experiencias', 'post type general name', 'factoria-cruzcampo-core' ),
-			'singular_name'      => _x( 'Experiencia', 'post type singular name', 'factoria-cruzcampo-core' ),
-			'menu_name'          => __( 'Experiencias', 'factoria-cruzcampo-core' ),
-			'add_new_item'       => __( 'Añadir nueva experiencia', 'factoria-cruzcampo-core' ),
-			'edit_item'          => __( 'Editar experiencia', 'factoria-cruzcampo-core' ),
-			'not_found'          => __( 'No se encontraron experiencias', 'factoria-cruzcampo-core' ),
+			'name'                     => _x( 'Experiencias', 'post type general name', 'factoria-cruzcampo-core' ),
+			'singular_name'            => _x( 'Experiencia', 'post type singular name', 'factoria-cruzcampo-core' ),
+			'menu_name'                => __( 'Experiencias', 'factoria-cruzcampo-core' ),
+			'add_new_item'             => __( 'Añadir nueva experiencia', 'factoria-cruzcampo-core' ),
+			'edit_item'                => __( 'Editar experiencia', 'factoria-cruzcampo-core' ),
+			'not_found'                => __( 'No se encontraron experiencias', 'factoria-cruzcampo-core' ),
+			'item_link'                => _x( 'Enlace a experiencia', 'navigation link block title', 'factoria-cruzcampo-core' ),
+			'item_link_description'    => _x( 'Un enlace a una experiencia', 'navigation link block description', 'factoria-cruzcampo-core' ),
 		);
 
 		register_post_type( 'experience', array(
-			'labels'       => $labels,
-			'public'       => true,
-			'has_archive'  => false,
-			'rewrite'      => array( 'slug' => 'experiencia' ),
-			'supports'     => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
-			'menu_icon'    => 'dashicons-star-filled',
-			'show_in_rest' => true,
+			'labels'              => $labels,
+			'public'              => true,
+			'has_archive'         => false,
+			'rewrite'             => array( 'slug' => 'experiencia' ),
+			'supports'            => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
+			'menu_icon'           => 'dashicons-star-filled',
+			'show_in_rest'        => true,
+			'show_in_nav_menus'   => true,
 		) );
 
 		register_post_meta( 'experience', 'product_id', array(
